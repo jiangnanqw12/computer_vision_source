99c97f059e261a18c2773504947df36fea9894b1
shade <382961818@qq.com>
2023-03-01 21:37:58 +0800

    new file:   Feature detection and matching/BRISK/BRISK_opencv.py

---
+++ Feature detection and matching/BRISK/BRISK_opencv.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Load the image
+img = cv2.imread('img1.png')
+
+# Create BRISK detector and descriptor
+brisk = cv2.BRISK_create()
+
+# Detect keypoints
+keypoints = brisk.detect(img, None)
+
+# Compute descriptors
+keypoints, descriptors = brisk.compute(img, keypoints)
+
+# Draw keypoints on the image
+img_with_keypoints = cv2.drawKeypoints(img, keypoints, None)
+
+# Display the image with keypoints
+cv2.imshow('BRISK keypoints', img_with_keypoints)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
